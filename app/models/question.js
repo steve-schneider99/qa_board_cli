4703ed34ef8dcd349aa504fa4cc540be5ed2687a
Steve <steve.schneider99@gmail.com>
2015-06-19 09:42:06 -0700

    Added initial files for setup. duplicated landing page from previous example for consistency.

---
+++ app/models/question.js
@@ -0,0 +1,8 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  question: DS.attr('string'),
+  author: DS.attr('string'),
+  description: DS.attr('string'),
+  answers: hasMany('answers', {async: true})
+});
